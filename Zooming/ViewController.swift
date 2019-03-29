337b01e2f92fd448644feeea4283b31f7b6dc05e
West Kraemer <kraemerwest@gmail.com>
2019-03-29 16:20:43 -0400

    finalize zoom ability of app.

--- Zooming/ViewController.swift
+++ Zooming/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIScrollViewDelegate {
 
     @IBOutlet weak var scrollView: UIScrollView!
     
@@ -17,10 +17,20 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       
-        
+        scrollView.delegate = self
+        updateZoomFor(size: view.bounds.size)
     }
 
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+    
+    func updateZoomFor(size: CGSize) {
+        let widthScale = size.width / imageView.bounds.width
+        let heightScale = size.height / imageView.bounds.height
+        let scale = min(widthScale, heightScale)
+        scrollView.minimumZoomScale = scale
+    }
 
 }
 
